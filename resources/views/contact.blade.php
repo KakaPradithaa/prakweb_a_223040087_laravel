dc6c2215c94f5ac04d2a8caadae3de5fc1e1fcff
KakaPradithaa <kakapradithaa123@gmail.com>
2024-11-02 14:20:44 +0700

    Vidio 5.Blade Component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <a href="/">Home</a>
-    <h1>Hubungi saya!</h1>
-    <a href="https://www.instagram.com/kaka.prdtha">Instagram</a>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman Contact</h3>
+</x-layout>
