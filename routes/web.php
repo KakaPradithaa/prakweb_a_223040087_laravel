83e2564f6b873e8918ceea0d0e700d96d711c696
KakaPradithaa <kakapradithaa123@gmail.com>
2024-11-03 19:29:12 +0700

    Vidio 16. Searching

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,7 @@ Route::get('/categories/{category:slug}', function (Category $category) {
 });
 
 Route::get('/posts', function () {
-    // $posts = Post::with(['author', 'category'])->latest()->get(); 
-    $posts = Post::latest()->get();
-    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
 });
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
