adce636e3fbfb9b5203bd26b387ee2c9bf2f0948
KakaPradithaa <kakapradithaa123@gmail.com>
2024-11-03 15:00:33 +0700

    Vidio 14. N + 1 Problem

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,20 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 Route::get('/authors/{user:username}', function (User $user) {
 
-    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
+    // $posts = $user->posts->load('category', 'author');
+    return view('posts', ['title' => count( $user->posts) . ' Articles by ' . $user->name, 'posts' =>  $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
 
+    // $posts = $category->posts->load('category', 'author');
     return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // $posts = Post::with(['author', 'category'])->latest()->get(); 
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
